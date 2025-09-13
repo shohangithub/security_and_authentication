81c6cc1089fe6d23229d30310ecda69a1e4dc1f2
Rafiul Islam <sohan@redlimesolutions.com>
2025-09-13 16:25:44 +0600

    add necessary pakages, register db service and enpoints

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Configure Entity Framework with an in-memory database
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseInMemoryDatabase("InMemoryDb"));
+
+
+// Configure Identity services and link to Entity Framework stores
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDbContext>();
+
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorizationBuilder();
 
 var app = builder.Build();
 
 
-// app.UseHttpsRedirection();
 
-app.MapGet("/", () => "root");
+app.MapGet("/", () => "I am root");
+app.MapGet("/admin-only", () => "admin access only").RequireAuthorization();
+app.MapGet("/user-claim-check", () => "Access with specific claim").RequireAuthorization();
+
+
+var roles = new[] { "Admin", "User" };
+
+
+
 
 app.Run();
+  
